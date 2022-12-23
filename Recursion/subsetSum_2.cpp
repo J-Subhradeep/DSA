a4f5e2171ec48900f2d3f3f78b8d3481990b86b1
Subhradeep Pal <subhradeeppal30@gmail.com>
2022-12-23 19:17:56 +0530

    subsetSum 2 corrected

--- Recursion/subsetSum_2.cpp
+++ Recursion/subsetSum_2.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 void subseqSum(int index, vector<int> arr, vector<int> ds, int sum)
 {
-    if (index == arr.size())
+
+    for (auto &&i : ds)
     {
-        for (auto &&i : ds)
-        {
-            cout << "+" << i;
-        }
-        cout << "=" << sum << endl;
-        return;
+        cout << "+" << i;
     }
+    cout << "=" << sum << endl;
+
     for (int i = index; i < arr.size(); i++)
     {
-
+        if (i > index && arr[i] == arr[i - 1])
+            continue;
         ds.push_back(arr[i]);
+
         subseqSum(i + 1, arr, ds, sum + arr[i]);
         ds.pop_back();
     }
