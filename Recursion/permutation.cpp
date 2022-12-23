1469f71b170e78887a8a04fdd44052a83a87893f
Subhradeep Pal <subhradeeppal30@gmail.com>
2022-12-23 21:24:37 +0530

    permutation

---
+++ Recursion/permutation.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+void permutation(int index, vector<int> arr, vector<int> ds, vector<int> mapper)
+{
+    if (ds.size() == arr.size())
+    {
+        for (auto &&i : ds)
+        {
+            cout << i << ",";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (!mapper[i])
+        {
+            mapper[i] = 1;
+            ds.push_back(arr[i]);
+            permutation(i, arr, ds, mapper);
+            ds.pop_back();
+            mapper[i] = 0;
+        }
+    }
+}
+void generate(vector<int> arr, vector<int> ds, vector<int> mapper)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        permutation(i, arr, ds, mapper);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    vector<int> arr{1, 2, 3, 4};
+    vector<int> mapper(arr.size(), 0);
+    permutation(0, arr, vector<int>(), mapper);
+    return 0;
+}
